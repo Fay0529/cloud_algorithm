a3294702167694163995416d43e9cbaf0ac24ab4
xFayChen <nicholasc@qq.com>
2018-02-23 21:44:02 +0800

    公司描述+工作年限提取

---
+++ company_description/company_description.py
@@ -0,0 +1,53 @@
+# !/usr/bin/env python3
+# -*- coding:utf-8*-
+
+
+import jieba
+import jieba.analyse
+import pandas as pd
+import numpy as np
+import pymysql
+#连接数据库
+db = pymysql.connect(host='localhost',
+                     user='root',
+                     password='123456',
+                     db='cloud',
+                     charset='utf8mb4')
+cursor=db.cursor()
+
+
+# 加载文件函数
+def loadData(path):
+    df=pd.read_csv(path,sep='\t')
+    return df
+df=loadData('/home/fay/workspace/python/select_company_name_company_description_.tsv')
+df.columns = ['name', 'description']
+#去重
+df1=df.drop_duplicates('name')
+# dfTest=df1.head(10);
+dfTest=df1
+#载入自定义词典
+jieba.load_userdict('/home/fay/workspace/python/dict.txt')
+#载入停词表
+jieba.analyse.set_stop_words('/home/fay/workspace/python/stopwords.txt')
+#print(type(dfTest))
+try:
+    count=1
+    f = open("/home/fay/workspace/python/out.txt", "w")
+    for i in dfTest.index:
+        chinese_keywords=jieba.analyse.textrank(dfTest.loc[i].values[1],withWeight=True, topK=30,allowPOS=('ns','n', 'vn'))
+        # print(count,file=f)
+        company_name=dfTest.loc[i].values[0]
+        for word in chinese_keywords:
+            # print('%s,%s'%(word[0],word[1]),file=f)
+            sql = "INSERT INTO company_description_output(company_name,word,weight) VALUES ('%s','%s','%s')"  %(company_name,word[0],word[1]*5)
+            cursor.execute(sql)
+            db.commit()
+            print('%s,%s,%s'%(company_name,word[0],word[1]*5),file=f)
+
+except Exception as err:
+    print(err)
+finally :
+    f.close()
+    db.close()
+    print("Connection closed")
---
+++ job_five_factors/job_exp.py
@@ -0,0 +1,50 @@
+# !/usr/bin/env python3
+# -*- coding:utf-8*-
+import pandas as pd
+import numpy as np
+import pymysql
+import re
+def filter(str):
+    m=re.match(r'(\d*)-*(\d*)',str)
+    if m.group(1) != '':
+        low=int(m.group(1))
+    else:
+        low=0
+    if m.group(2)!= '':
+        high=int(m.group(2))
+    else:
+        high=low
+    average=(high+low)/2
+    return low,high,average
+#连接数据库
+db = pymysql.connect(host='localhost',
+                    user='root',
+                    password='123456',
+                    db='cloud',
+                    charset='utf8mb4')
+cursor=db.cursor()
+
+
+# 加载文件函数
+def loadData(path):
+    df=pd.read_csv(path,sep='\t')
+    return df
+df=loadData('/home/fay/workspace/python/select_job_id_job_experience_from_job2_0.tsv')
+df.columns = ['job_id', 'exp']
+# dfTest=df.head(10);
+dfTest=df
+try:
+    for i in dfTest.index:
+        l=filter(dfTest.loc[i].values[1])
+        # print(count,file=f)
+        job_id=dfTest.loc[i].values[0]
+        sql = "INSERT INTO job_five_factors(job_id,job_exp_low,job_exp_high,job_exp_average) VALUES ('%s','%s','%s','%s')"  %(job_id,l[0],l[1],l[2])
+        cursor.execute(sql)
+        db.commit()
+
+
+except Exception as err:
+    print(err)
+finally :
+    db.close()
+    print("Connection closed")
